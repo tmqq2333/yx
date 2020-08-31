34b4e0f5fcdb0f93ef9645aec5f8f1144623edd1
tmqq <69976139+tmqq2333@users.noreply.github.com>
2020-08-31 14:39:26 +0800

    Add files via upload

---
+++ cooker/cooker.js
@@ -0,0 +1,90 @@
+// pages/cooker/cooker.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    chushilist:[]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+      var that=this;
+      that.setData({
+        urlhead:getApp().globalData.urlhead
+      });
+        //在此处编写请求首页菜品数据的逻辑代码
+      wx.request({
+        url: getApp().globalData.urlhead+'/dcapi/chushilist',
+        method: 'POST',
+        data: {
+          
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded' // 默认值
+        },
+        dataType: 'json',
+        success(cc) {
+          console.log(cc);
+          that.setData({
+            chushilist:cc.data
+          });
+        }
+      });
+ 
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
